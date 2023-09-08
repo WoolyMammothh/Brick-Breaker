eecd58ac033d4abb552742333a19739e03c53e0d
WoolyMammothh <davidojobo1@gmail.com>
2023-09-08 10:10:40 +0100

    Added funny character to game

--- bricks.py
+++ bricks.py
@@ -1,11 +1,17 @@
 import pygame
+import os
 
-BLACK = (0, 0, 0)
+BLACK, WHITE = (0, 0, 0), (255, 255, 255)
 WIDTH, HEIGHT = 1000, 600
+CHAR_HEIGHT, CHAR_WIDTH = 50, 50
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+# paddle = pygame.draw.rect(screen, WHITE, (100, 50))
+char_image = pygame.image.load(os.path.join('Assets', 'character.png'))
+guy = pygame.transform.scale(char_image, (CHAR_WIDTH, CHAR_HEIGHT))
 
 def update_display():
   screen.fill(BLACK)
+  screen.blit(guy, ((WIDTH//2)-(CHAR_WIDTH//2), (HEIGHT//2)-(CHAR_HEIGHT//2)))
   pygame.display.update()
 
 def main():
