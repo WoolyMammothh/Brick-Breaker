1494b53bfbae6d8ce0687bf264412341e8a25330
WoolyMammothh <davidojobo1@gmail.com>
2023-09-08 10:25:15 +0100

    added rectangle to represent character position

--- bricks.py
+++ bricks.py
@@ -7,14 +7,16 @@ CHAR_HEIGHT, CHAR_WIDTH = 50, 50
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 # paddle = pygame.draw.rect(screen, WHITE, (100, 50))
 char_image = pygame.image.load(os.path.join('Assets', 'character.png'))
-guy = pygame.transform.scale(char_image, (CHAR_WIDTH, CHAR_HEIGHT))
+character = pygame.transform.scale(char_image, (CHAR_WIDTH, CHAR_HEIGHT))
 
-def update_display():
+def update_display(pink):
   screen.fill(BLACK)
-  screen.blit(guy, ((WIDTH//2)-(CHAR_WIDTH//2), (HEIGHT//2)-(CHAR_HEIGHT//2)))
+  screen.blit(character, (pink.x, pink.y))
   pygame.display.update()
 
 def main():
+  pink = pygame.Rect((WIDTH//2)-(CHAR_WIDTH//2), (HEIGHT//2)-(CHAR_HEIGHT//2), 
+                     CHAR_WIDTH, CHAR_HEIGHT)
   fps = pygame.time.Clock()
   running = True
   while running:
@@ -22,7 +24,7 @@ def main():
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         running = False
-    update_display()
+    update_display(pink)
 
   pygame.quit()
 
